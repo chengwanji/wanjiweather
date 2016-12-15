954ab06cf635724f76416bef67bd6b659c61bfb5
chengwanji <1107212387@qq.com>
2016-12-15 16:19:33 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/wanjiweather/util/HttpUtil.java
@@ -0,0 +1,15 @@
+package com.example.wanjiweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by WJ on 2016/12/14.
+ */
+public class HttpUtil {
+    public  static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
